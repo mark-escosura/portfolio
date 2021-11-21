541555ced59e11bad066ad39935f32c4af56eb43
mark-escosura <escosuramarkse@gmail.com>
2021-11-21 15:57:59 -0800

    Used Route Method to successfully route each component to its right path

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
+// methods
+import { Route, Switch } from "react-router";
 import { ThemeProvider } from "styled-components";
-import "./App.css";
 import { lightTheme } from "./components/Themes";
 import GlobalStyle from "./GlobalStyles";
+import "./App.css";
+
+// components
+import AboutPage from "./components/AboutPage";
+import BlogPage from "./components/BlogPage";
+import Main from "./components/Main";
+import MySkillsPage from "./components/MySkillsPage";
+import WorkPage from "./components/WorkPage";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
 
-      <ThemeProvider theme={lightTheme}>App File</ThemeProvider>
+      <ThemeProvider theme={lightTheme}>
+
+      <Switch>
+        <Route exact path="/" component={Main}/>
+        <Route exact path="/about" component={AboutPage}/>
+        <Route exact path="/blog" component={BlogPage}/>
+        <Route exact path="/work" component={WorkPage}/>
+        <Route exact path="/skills" component={MySkillsPage}/>
+      </Switch>
+      </ThemeProvider>
     </div>
   );
 }
