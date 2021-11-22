fcfbe900d2d602700ed652c49d8fe31f4c5aeadd
mark-escosura <escosuramarkse@gmail.com>
2021-11-22 00:35:52 -0800

    Created Links for Contact, Blog, Work, About, and Skills that leads to a its designated path

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-
+import { NavLink } from "react-router-dom";
 // components
 import LogoComponent from "../subComponents/LogoComponent";
 import PowerButton from "../subComponents/PowerButton";
@@ -28,6 +28,58 @@ const Container = styled.div`
   padding: 2rem;
 `;
 
+const BottomBar = styled.div`
+  position: absolute;
+  bottom: 1rem;
+  left: 0;
+  right: 0;
+  width: 100%;
+
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+const Contact = styled(NavLink)`
+  color: ${(props) => props.theme.text};
+  position: absolute;
+  top: 2rem;
+  right: calc(1rem + 2vw);
+  text-decoration: none;
+  z-index: 1;
+`;
+
+const BLOG = styled(NavLink)`
+  color: ${(props) => props.theme.text};
+  position: absolute;
+  top: 50%;
+  right: calc(1rem + 2vw);
+  transform: rotate(90deg) translate(-50%, -50%);
+  text-decoration: none;
+  z-index: 1;
+`;
+
+const WORK = styled(NavLink)`
+  color: ${(props) => props.theme.text};
+  position: absolute;
+  top: 50%;
+  left: 3rem;
+  transform: rotate(270deg) translate(-50%, -50%);
+  text-decoration: none;
+  z-index: 1;
+`;
+
+const ABOUT = styled(NavLink)`
+  color: ${(props) => props.theme.text};
+  text-decoration: none;
+  z-index: 1;
+`;
+
+const SKILLS = styled(NavLink)`
+  color: ${(props) => props.theme.text};
+  text-decoration: none;
+  z-index: 1;
+`;
+
 const Main = () => {
   return (
     <MainContainer>
@@ -35,6 +87,31 @@ const Main = () => {
         <LogoComponent />
         <PowerButton />
         <SocialIcons />
+
+        <Contact
+          target="_blank"
+          to={{ pathname: "mailto:escosuramarkse@gmail.com" }}
+        >
+          <h2>Say hi..</h2>
+        </Contact>
+
+        <BLOG to="/blog">
+          <h2>Blog</h2>
+        </BLOG>
+
+        <WORK to="/work">
+          <h2>Wok</h2>
+        </WORK>
+
+        <BottomBar>
+          <ABOUT to="/about">
+            <h2>About.</h2>
+          </ABOUT>
+
+          <SKILLS to="/skills">
+            <h2>My Skills.</h2>
+          </SKILLS>
+        </BottomBar>
       </Container>
     </MainContainer>
   );
