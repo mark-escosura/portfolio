3d7e600fd75608e4d94374418a2df677c6f12450
mark-escosura <escosuramarkse@gmail.com>
2021-11-22 11:49:29 -0800

    Created YingYang Button with transitions

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,11 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
+import styled, { keyframes } from "styled-components";
 import { NavLink } from "react-router-dom";
 // components
 import LogoComponent from "../subComponents/LogoComponent";
 import PowerButton from "../subComponents/PowerButton";
 import SocialIcons from "../subComponents/SocialIcons";
+import { YinYang } from "./AllSvgs";
 
 const MainContainer = styled.div`
   background: ${(props) => props.theme.body};
@@ -69,7 +70,7 @@ const WORK = styled(NavLink)`
 `;
 
 const ABOUT = styled(NavLink)`
-  color: ${(props) => props.theme.text};
+  color: ${(props) => props.click ? props.theme.body : props.theme.text };
   text-decoration: none;
   z-index: 1;
 `;
@@ -80,13 +81,78 @@ const SKILLS = styled(NavLink)`
   z-index: 1;
 `;
 
+const rotate = keyframes`
+from {
+  transform: rotate(0);
+}
+to {
+  transform: rotate(360deg);
+}
+`;
+
+const Center = styled.button`
+  position: absolute;
+  top: ${(props) => (props.click ? "85%" : "50%")};
+  left: ${(props) => (props.click ? "92%" : "50%")};
+  transform: translate(-50%, -50%);
+  border: none;
+  outline: none;
+  background-color: transparent;
+  cursor: pointer;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  transition: all 1s ease-in-out;
+
+  & > :first-child {
+    animation: ${rotate} infinite 1.5s linear;
+  }
+
+  & > :last-child {
+    display: ${(props) => (props.click ? "none" : "inline-block")};
+    padding-top: 1rem;
+  }
+`;
+
+const DarkDiv = styled.div`
+  position: absolute;
+  background-color: ${props => props.theme.text};
+  top: 0;
+  bottom: 0;
+  right: 50%;
+  width: ${props => props.click ? '50%' : '0%'};
+  height: ${props => props.click ? '100%' : '0%'};
+  z-index: 1;
+
+  transition: height 0.5s ease, width 1s ease 0.5s;
+`;
+
 const Main = () => {
+  // state
+  const [click, setClick] = useState(false);
+
+  // handlers
+  const handleClick = () => setClick(!click);
+
   return (
     <MainContainer>
+      <DarkDiv click={click} />
       <Container>
         <LogoComponent />
         <PowerButton />
         <SocialIcons />
+        <Center click={click}>
+          <YinYang
+            onClick={handleClick}
+            width={click ? 120 : 200}
+            height={click ? 120 : 200}
+            fill="currentColor"
+          />
+          <span>click here</span>
+        </Center>
 
         <Contact
           target="_blank"
@@ -99,12 +165,12 @@ const Main = () => {
           <h2>Blog</h2>
         </BLOG>
 
-        <WORK to="/work">
+        <WORK to="/work" click={click}>
           <h2>Wok</h2>
         </WORK>
 
         <BottomBar>
-          <ABOUT to="/about">
+          <ABOUT to="/about" click={click}>
             <h2>About.</h2>
           </ABOUT>
 
