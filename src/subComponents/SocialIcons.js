97d260722952a4769ddf08a00e5056415bb7ada8
mark-escosura <escosuramarkse@gmail.com>
2021-11-23 12:37:18 -0800

    Used Prettier to Format Document to make it look clean

--- src/subComponents/SocialIcons.js
+++ src/subComponents/SocialIcons.js
@@ -3,55 +3,88 @@ import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 // components
 import { Github, Twitter, Facebook, YouTube } from "../components/AllSvgs";
-import { DarkTheme } from '../components/Themes';
+import { DarkTheme } from "../components/Themes";
 
 const Icons = styled.div`
-display:flex;
-flex-direction: column;
-align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-position:fixed;
-bottom:0;
-left: 3rem;
+  position: fixed;
+  bottom: 0;
+  left: 3rem;
 
-z-index:3;
+  z-index: 3;
 
-&>*:not(last-child){
-      margin: 0.5rem 0;
+  & > *:not(last-child) {
+    margin: 0.5rem 0;
   }
-`
+`;
 
 const Line = styled.span`
-width: 2px;
-height: 8rem;
-background-color: ${props => props.color === 'dark' ? DarkTheme.text : DarkTheme.body};
-`
+  width: 2px;
+  height: 8rem;
+  background-color: ${(props) =>
+    props.color === "dark" ? DarkTheme.text : DarkTheme.body};
+`;
 
 const SocialIcons = (props) => {
   return (
     <Icons>
       <div>
-        <NavLink style={{color:'inherit'}} target="_blank" to={{pathname:"https://www.github.com/mark-escosura"}}>
-          <Github width={25} height={25} fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body } />
+        <NavLink
+          style={{ color: "inherit" }}
+          target="_blank"
+          to={{ pathname: "https://www.github.com/mark-escosura" }}
+        >
+          <Github
+            width={25}
+            height={25}
+            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
+          />
         </NavLink>
       </div>
       <div>
-        <NavLink style={{color:'inherit'}} target="_blank" to={{pathname:"https://www.twitter.com"}}>
-          <Twitter width={25} height={25} fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body } />
+        <NavLink
+          style={{ color: "inherit" }}
+          target="_blank"
+          to={{ pathname: "https://www.twitter.com" }}
+        >
+          <Twitter
+            width={25}
+            height={25}
+            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
+          />
         </NavLink>
       </div>
       <div>
-        <NavLink style={{color:'inherit'}} target="_blank" to={{pathname:"https://www.facebook.com"}}>
-          <Facebook width={25} height={25} fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body } />
+        <NavLink
+          style={{ color: "inherit" }}
+          target="_blank"
+          to={{ pathname: "https://www.facebook.com" }}
+        >
+          <Facebook
+            width={25}
+            height={25}
+            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
+          />
         </NavLink>
       </div>
       <div>
-        <NavLink style={{color:'inherit'}} target="_blank" to={{pathname:"https://www.youtube.com"}}>
-          <YouTube width={25} height={25} fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body } />
+        <NavLink
+          style={{ color: "inherit" }}
+          target="_blank"
+          to={{ pathname: "https://www.youtube.com" }}
+        >
+          <YouTube
+            width={25}
+            height={25}
+            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
+          />
         </NavLink>
       </div>
 
-      <Line color={props.theme}/>
+      <Line color={props.theme} />
     </Icons>
   );
 };
