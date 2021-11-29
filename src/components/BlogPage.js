82efcf5e5e04b5cdfae1aeb46a3fc64cb9b0363b
mark-escosura <escosuramarkse@gmail.com>
2021-11-28 23:42:13 -0800

    Created a dummy data for the blog page and successfully got it rendered into the DOM. I used styled components as a styling tool

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,11 +1,67 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import img from "../assets/images/blog-background-image.png";
+
+// components
+import LogoComponent from "../subComponents/LogoComponent";
+import SocialIcons from "../subComponents/SocialIcons";
+import PowerButton from "../subComponents/PowerButton";
+import BlogComponent from "../components/BlogComponent";
+
+// data
+import { Blogs } from "../data/BlogData";
+
+const MainContainer = styled.div`
+  background-image: url(${img});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Container = styled.div`
+  background-color: ${(props) => `rgba(${props.theme.bodyRgba},0.8)`};
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  padding-bottom: 5rem;
+`;
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 10rem;
+`;
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, minmax(calc(10rem + 15vw), 1fr));
+  grid-gap: calc(1rem + 2vw);
+`;
 
 const BlogPage = () => {
-    return (
-        <div>
-            Blog Component/Page
-        </div>
-    )
-}
+  return (
+    <MainContainer>
+      <Container>
+        <LogoComponent />
+        <PowerButton />
+        <SocialIcons />
+        <Center>
+          <Grid>
+            {
+                Blogs.map(blog => {
+                    return (
+                    <BlogComponent key={blog.id} blog={blog} />
+                    )
+                })
+            }
+          </Grid>
+        </Center>
+      </Container>
+    </MainContainer>
+  );
+};
 
-export default BlogPage
+export default BlogPage;
---
+++ src/data/BlogData.js
@@ -0,0 +1,29 @@
+export const Blogs = [
+  {
+    id: 1,
+    name: "Blog Test Number One",
+    tags: ["react", "javascript"],
+    date: "26 June 2021",
+    imgSrc:
+      "https://images.unsplash.com/photo-1580927752452-89d86da3fa0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y29kaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    link: "#",
+  },
+  {
+    id: 2,
+    name: "Blog Test Number Two",
+    tags: ["react", "javascript", "styled-components"],
+    date: "20 October 2021",
+    imgSrc:
+      "https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=755&q=80",
+    link: "#",
+  },
+  {
+    id: 3,
+    name: "Blog Test Number Three",
+    tags: ["css", "html"],
+    date: "16 September 2021",
+    imgSrc:
+      "https://images.unsplash.com/photo-1546146830-2cca9512c68e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+    link: "#",
+  },
+];
