e235218dcfb9de953dd8c4f32f4efce721863810
mark-escosura <escosuramarkse@gmail.com>
2022-01-06 00:33:15 -0800

    Work Page Completed

--- src/components/WorkPage.js
+++ src/components/WorkPage.js
@@ -1,68 +1,50 @@
-import React from "react";
-import styled, { /*keyframes ,*/ ThemeProvider } from "styled-components";
+import React, { useRef, useEffect } from "react";
+import styled, { ThemeProvider } from "styled-components";
 import { DarkTheme } from "./Themes";
 
+import Card from "../subComponents/Card";
 import LogoComponent from "../subComponents/LogoComponent";
 import SocialIcons from "../subComponents/SocialIcons";
 import PowerButton from "../subComponents/PowerButton";
 import ParticleComponent from "../subComponents/ParticleComponent";
- 
+
+import { Work } from "../data/WorkData.js";
+
 // import caspar from "../assets/images/Caspar.png"
 
 const Box = styled.div`
   background-color: ${(props) => props.theme.body};
-  width: 100vw;
-  height: 100vh;
+  height: 400vh;
   position: relative;
   overflow: hidden;
   cursor: pointer;
 `;
 
-// const float = keyframes
-// 0% { transform: translateY(-10px)}
-// 50% { transform: translateY(-20px) translateX(20px)}
-// 100% { transform: translateY(-10px)}
-// `;
+const Main = styled.ul`
+  position: fixed;
+  top: 12rem;
+  left: calc(10rem + 15vw);
+  height: 40vh;
 
-// const Caspar = styled.div`
-//   position: absolute;
-//   top: 25%;
-//   right : -12%;
-//   width: 60vw;
-//   animation: ${float} 5s ease infinite;
-//   img {
-//     width: 100%;
-//     height: auto;
-//   }
-// `;
+  display: flex;
+  color: ${(props) => props.theme.text};
+`;
+
+const WorkPage = () => { 
+  const ref = useRef(null)
+
+  useEffect(() => {
+ let element = ref.current;
+
+ const rotate = () => {
+   element.style.transform = `translateX(${-window.pageYOffset}px)`
+ }
+
+ window.addEventListener('scroll', rotate)
+ return () => window.removeEventListener('scroll', rotate);
+  }, [])
 
 
-
-const Main = styled.div`
-    border: 2px solid ${props => props.theme.text};
-    border-radius: 2rem 0 2rem 0;
-    color: ${props => props.theme.text};
-    padding: 2rem;
-    width: 15vw;
-    height: 40vh;
-    z-index: 3;
-    line-height: 1.5;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: calc(0.6rem + 1vw);
-    backdrop-filter: blur(4px);
-
-    position: absolute;
-    left: calc(5rem + 5vw);
-    top: 25%;
-
-    font-family: 'Ubuntu Mono', monospace;
-    font-style: italic;
-`
-
-const WorkPage = () => {
   return (
     <ThemeProvider theme={DarkTheme}>
       <Box>
@@ -71,8 +53,12 @@ const WorkPage = () => {
         <PowerButton />
         <ParticleComponent theme="dark" />
 
-        <Main>
-            workpage
+        <Main ref={ref}>
+          {
+          Work.map(work => {
+              return <Card key={work.id} data={work}>Work Data</Card>;
+          })
+          }
         </Main>
       </Box>
     </ThemeProvider>
---
+++ src/subComponents/Card.js
@@ -0,0 +1,152 @@
+import React from "react";
+import { motion } from "framer-motion";
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+import { Github } from "../components/AllSvgs";
+
+const Box = styled(motion.li)`
+  width: 16rem;
+  height: 40vh;
+  background-color: ${(props) => props.theme.text};
+  color: ${(props) => props.theme.body};
+  padding: 1.5rem 2rem;
+  margin-right: 8rem;
+  border-radius: 0 50px 0 50px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border: 1px solid ${(props) => props.theme.body};
+
+  transition: all 1s ease;
+
+  &:hover {
+    background-color: ${(props) => props.theme.body};
+    color: ${(props) => props.theme.text};
+    border: 1px solid ${(props) => props.theme.text};
+    cursor: auto;
+  }
+`;
+
+const Title = styled.h2`
+  font-size: calc(1em + 0.5vw);
+`;
+
+const Description = styled.h2`
+  font-size: calc(0.8em + 0.4vw);
+  font-family: "Karla", sans-serif;
+  font-weight: 500;
+`;
+
+const Tags = styled.div`
+  border-top: 2px solid ${(props) => props.theme.body};
+  padding-top: 0.5rem;
+  display: flex;
+  flex-wrap: wrap;
+
+  ${Box}:hover &{
+    border-top: 2px solid ${(props) => props.theme.text};
+  }
+`;
+
+const Tag = styled.span`
+  margin-right: 1rem;
+  font-size: calc(0.8em + 0.3vw);
+`;
+
+const Footer = styled.footer`
+display: flex;
+justify-content: center;
+`
+
+const Link = styled(NavLink)`
+background-color: ${props => props.theme.body};
+color: ${props => props.theme.text};
+text-decoration: none;
+padding: 0.5rem calc(2rem + 2vw);
+border-radius: 0 0 0 50px;
+font-size: calc(1em + 0.5vw);
+cursor: pointer;
+
+${Box}:hover &{
+  background-color: ${props => props.theme.text};
+  color: ${props => props.theme.body};
+  border-radius: 0 50px 0 0;
+
+}
+`
+
+const Git = styled(NavLink)`
+color: inherit;
+text-decoration: none;
+margin-left: 2rem;
+cursor: pointer;
+
+${Box}:hover &{
+  &>*{
+    fill: ${props => props.theme.text};
+  }
+}
+`
+
+const Card = (props) => {
+  const { id, name, description, tags, demo, github } = props.data;
+  return (
+    <Box
+      key={id}
+      initial={{ height: 0 }}
+      animate={{ height: "40vh" }}
+      transition={{ type: "tween", duration: 1 }}
+      whileHover={{ scale: 1.1 }}
+    >
+      {/* Title */}
+      <Title>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.5, delay: 2 }}
+        >
+          {name}
+        </motion.div>
+      </Title>
+      {/* Description */}
+      <Description>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.5, delay: 2 }}
+        >
+          {description}
+        </motion.div>
+      </Description>
+      {/* Tags */}
+      <Tags>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.5, delay: 2 }}
+        >
+          {tags.map((t, id) => {
+            return <Tag key={id}>#{t}</Tag>;
+          })}
+        </motion.div>
+      </Tags>
+      <Footer>
+      <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.5, delay: 2 }}
+        >
+        <Link to={{pathname: `${demo}`}} target="_blank">
+          Visit
+        </Link>
+        <Git to={{pathname: `${demo}`}} target="_blank">
+          <Github width={30} height={30} />
+        </Git>
+        </motion.div>
+      </Footer>
+    </Box>
+  );
+};
+
+export default Card;
