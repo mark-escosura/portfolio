791909aa9138b41363b87812190a39bc20b01e9d
mark-escosura <escosuramarkse@gmail.com>
2022-03-07 20:27:50 -0800

    data: added budget-app project to my WorkData file

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,33 +1,33 @@
 export const Work = [
-    {
-        id:1,
-        name:"Log in page (WIP)",
-        description:"It is build on top of React JS, and styledComponents on the front-end and Express JS on the back-end.",
+  {
+    id: 1,
+    name: 'Private Budget App',
+    description:
+      'This private budget app project was bootstrapped with Create React App and Bootstrap',
 
-        tags:["react","styledComponents", "expressJS"],
-        
-        demo:"/",
-        github:"/"
-    },
-    {
-        id:2,
-        name:"Responsive Navigation Menu (WIP)",
-        description:"A responsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
-        tags:["react","sass"],
-        
-        demo:"/",
-        github:"/"
-        
-    },
-    {
-        id:3,
-        name:"Todo App(React + Redux)",
-        description:"A todo-app built using Redux-framework and framer-motion for smooth and cool animations.",
-        tags:["react","redux","framer-motion"],
-        
-        demo:"/",
-        github:"/"       
-    },
-    
-  
-] 
+    tags: ['react', 'bootstrap', 'context-api'],
+
+    demo: 'https://private-budget-app.vercel.app/',
+    github: 'https://github.com/mark-escosura/budget-app',
+  },
+  {
+    id: 2,
+    name: 'Responsive Navigation Menu (WIP)',
+    description:
+      'A responsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.',
+    tags: ['react', 'sass'],
+
+    demo: '/',
+    github: '/',
+  },
+  {
+    id: 3,
+    name: 'Todo App(React + Redux)',
+    description:
+      'A todo-app built using Redux-framework and framer-motion for smooth and cool animations.',
+    tags: ['react', 'redux', 'framer-motion'],
+
+    demo: '/',
+    github: '/',
+  },
+];
